d8141e5cef3bb9fb98a662334d370f2c56122761
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-11-05 20:46:44 -0400

    generate configs

--- vps.go
+++ vps.go
@@ -1,26 +1,70 @@
 package main
 
 import (
+    "fmt"
     "os"
 )
+
+const (
+    RAM_LOW    = 512
+    RAM_MEDIUM = 1024
+    RAM_HIGH   = 2048
+)
+
+type osInfo struct {
+    ImageFile     string
+}
+
+const osOptions = map[string]osInfo{
+    'ubuntu':    osInfo{ImageFile: "ubuntu-server.img"}
+}
 
 type VPSConfig struct {
     Hostname      string
     Username      string
     Password      string
     SSHKey        string
+    RAM           int // make this 'enum' or sm
+    OS            string
 }
 
-func VPSCreate() {
+func VPSCreate(config VPSConfig) {
+
+    // TODO do some validation on config to make sure there is no
+    // script injection or insane settings going on
 
     // generate user-data and meta-data files
+    fmt.Sprintf(`
+        instance-id: %s
+        local-hostname: %s
+    `, config.Hostname, config.Hostname)
 
-    // create cidata image
+    fmt.Sprintf(`
+        #cloud-config
+        users:
+          - name: %s
+            ssh_authorized_keys:
+              - %s
+            groups: sudo
+            shell: /bin/bash
+            passwd: %s
+            lock_passwd: false
+    `, config.Username, config.SSHKey, config.Password)
+
+    // create cidata image (maybe do it in /temp ?)
+    cidataLocation := "cidata.iso"
+    userdataLocation := "user-data"
+    metadataLocation := "meta-data"
+
+    cmd := []string{
+        "genisoimage", "-output", cidataLocation, "-V",
+        "cidata", "-r", "-J", userdataLocation, metadataLocation
+    }
 
     // create disk image
 
     // create the vm
-    cmd := []string{
+    cmd = []string{
         "virt-install"
     }
 
